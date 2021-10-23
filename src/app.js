b5512c5a17b10bcd2d8f26e31fe1dd98454b9160
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-10-23 08:40:36 -0500

    Scaffolding API end points

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/hello', (req, res)=> res.send('Hello!'));
+app.get('/', (req, res)=> res.send('Hello!'));
+
+app.get('/member', (req, res)=> res.send('Hello!'));
+
+app.get('/appoitment', (req, res)=> res.send('Hello!'));
+
+app.get('/history', (req, res)=> res.send('Hello!'));
+
+app.get('/bill', (req, res)=> res.send('Hello!'));
+
+app.get('/query', (req, res)=> res.send('Hello!'));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
