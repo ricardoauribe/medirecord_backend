f099eedc4d2fe819e062b028acbf10ca820a5cf7
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-10-24 07:37:33 -0500

    Additional info on end points

--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res)=> res.send('Hello!'));
+app.get('/', (req, res)=> res.send('Welcome to the family medical record!'));
 
-app.get('/member', (req, res)=> res.send('Hello!'));
+app.get('/member', (req, res)=> res.send('Member Information!'));
 
-app.get('/appoitment', (req, res)=> res.send('Hello!'));
+app.get('/appoitment', (req, res)=> res.send('Appointment Information!'));
 
-app.get('/history', (req, res)=> res.send('Hello!'));
+app.get('/history', (req, res)=> res.send('History Records for a family member!'));
 
-app.get('/bill', (req, res)=> res.send('Hello!'));
+app.get('/bill', (req, res)=> res.send('Bill instance!'));
 
-app.get('/query', (req, res)=> res.send('Hello!'));
+app.get('/query', (req, res)=> res.send('Section to make complex queries!'));
 
 app.listen(8000, () => console.log('Listening on port 8000'));
